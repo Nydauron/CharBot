8f4b0617f53480082a8428b94e9b3208bba82dce
Jareth Gomes <jarethgomes@gmail.com>
2019-12-12 14:57:26 -0600

    Made the fields in the config class private

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     def __init__(self, config_json_path):
         with open(config_json_path, 'r') as f:
             config = json.load(f)
-            self.token = config["token"]
+            self.__token = config["token"]
 
     def getToken(self):
-        return self.token
+        return self.__token
