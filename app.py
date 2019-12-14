fb5d5e9c3e11b8582b0ba67dc7ddcb48291ab8c4
Jareth Gomes <jarethgomes@gmail.com>
2019-12-13 23:53:34 -0600

    Added guild and channel listeners

--- app.py
+++ app.py
@@ -14,6 +14,25 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
+    async def on_guild_join(self, guild):
+        # This needs to be able to generate the default settings based off the guild and channel ids
+        # This also needs to identify to NOT generate new settings if there already exists settings for that server
+        # Settings should revolve around VERSIONS so that if in future I update any part of the data structure, the bot
+        #     boot up will rebuild the settings WHILE maintaining integrity of the data previously stored
+        print('I joined a guild!')
+        if not settings.does_guild_have_existing_settings(guild):
+            settings.generate_default_settings(guild)
+
+    async def on_guild_remove(self, guild):
+        # I am deciding to KEEP the settings stored instead of deleting them in the event the bot gets removed and then
+        #     is readded
+        print('I was removed from a guild')
+
+    async def on_guild_channel_create(self, channel):
+        print(channel)
+        if not settings.does_guild_have_existing_settings(channel.guild):
+            settings.generate_default_settings(channel.guild)
+
 client = MyClient()
 client.run(config.getToken())
 
