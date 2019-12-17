e7cd38cb32b8a204d092ad33a09ea546595ceb93
Jareth Gomes <jarethgomes@gmail.com>
2019-12-16 18:03:22 -0600

    Added details mentioning how voting for roasts will work

--- roast.py
+++ roast.py
@@ -5,6 +5,15 @@ class Roast:
         self.__down_vote = 0
         self.__up_vote = 0
 
+    # For voting, in order to prevent spamming and possible manipulation, we need to limit the amount of voting to be
+    # 1 vote per 24 hrs per roast
+    # Players may be able to change their vote, but this will reset the 24 hr time limit for them
+    # This will be accomplished by compiling an array of the userid and the timestamp that the user voted with
+    # If a user has removed his or her vote for a roast, then they are removed from that list and are eligible to revote
+    # Revoting will cause the 24 cooldown period to restart.
+    # After 24 hours, the users are removed from the list symbolizing that they can vote again.
+    # If after 24 hours has passed, then the user will be allowed to vote again for the same roast.
+
     def getRoast(self):
         return self.__roast_msg
 
--- roastlist.py
+++ roastlist.py
@@ -7,3 +7,10 @@ class RoastList:
         pickle_in = open(roasts_path, "rb")
         self.__list_of_roasts = pickle.load(pickle_in)
         pickle_in.close()
+
+        # When a roast is said, the bot will allow users to vote.
+        # It keeps note of what the messageid is of that roast and associates that id with the roast in the roast list
+        # Another list then keeps track of which users have voted. User voting data is NOT NECESSARILY needed since all
+        # we need is if they voted or not.
+        # Once a person votes, then they will have a 24 hour cooldown before they can vote for that roast again.
+        # After 24 hours, the roast that was said will no longer be up for voting.
