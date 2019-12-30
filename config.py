2b7375637ceb11c8704ef35171b16f2240e4388e
Jareth Gomes <35314805+Nydauron@users.noreply.github.com>
2019-12-29 18:47:36 -0600

    Added prefix and keyword attributes to config

--- config.py
+++ config.py
@@ -5,7 +5,15 @@ class Config:
     def __init__(self, config_json_path):
         with open(config_json_path, 'r') as f:
             config = json.load(f)
-            self.__token = config["token"]
+            self.__token = config['token']
+            self.__prefix = config['prefix']
+            self.__keyword = config['keyword']
 
     def getToken(self):
         return self.__token
+
+    def getPrefix(self):
+        return self.__prefix
+
+    def getKeyword(self):
+        return self.__keyword
